e0b6e41a4630c355d3eecb3ef93f630fe5ddb0a7
Heather <heather@thisisnotanorg.org>
2015-01-30 12:56:01 -0800

    Add working links to individual band names.

--- app.rb
+++ app.rb
@@ -13,3 +13,16 @@ post('/') do
   band = Band.create({ band_name: band_name })
   redirect '/'
 end
+
+get('/bands/:id') do
+  @band = Band.find(params["id"].to_i)
+  erb(:bands)
+end
+
+post('/bands/:id') do
+  band = Band.find(params["id"].to_i)
+  band_id = band.id()
+  redirect "/bands/#{band_id}"
+end
+
+#<a href="/bands/<%= band.id %>"<%= band.band_name %></a>
