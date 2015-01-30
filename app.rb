44b5eec88c726ffb4bbd956ad61218fa3b5f92d2
Heather <heather@thisisnotanorg.org>
2015-01-30 11:53:49 -0800

    Add index, form to add band, and simple css.

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each{ |file| require file }
 
 get('/') do
-  @bands = Bands.all()
+  @bands = Band.all
   erb(:index)
+end
+
+post('/') do
+  band_name = params["band_name"]
+  band = Band.create({ band_name: band_name })
+  redirect '/'
 end
--- lib/venue.rb
+++ lib/venue.rb
@@ -1,5 +1,11 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
   validates(:venue, { :presence => true, :length => { maximum: 100 } })
+  before_save(:titlecase_band_name)
 
+private
+
+  define_method(:titlecase_band_name) do
+    self.band_name=(band_name().titlecase())
+  end
 end
